de294a91dc010c53befe8ad69d1c3c0763dd46da
m4tthewz <141052332+m4tthewz@users.noreply.github.com>
2023-12-15 15:32:22 -0500

    finished!

--- PrototypeVex202324/src/main.cpp
+++ PrototypeVex202324/src/main.cpp
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/
+ /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
 /*    Author:       karan                                                     */
@@ -7,34 +7,44 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+
 #pragma region VEXcode Generated Robot Configuration
 // Make sure all required headers are included.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h> 
+#include <math.h>
 #include <string.h>
+
 
 #include "vex.h"
 
+
 using namespace vex;
+
+
+
 
 
 
 // A global instance of competition
 competition Competition;
 
+
 // define your global instances of motors and other devices here
+
 
 // START V5 MACROS
 #define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
-  } while (!(condition))
+ do {                                                                         \
+   wait(5, msec);                                                             \
+ } while (!(condition))
+
 
 #define repeat(iterations)                                                     \
-  for (int iterator = 0; iterator < iterations; iterator++)
+ for (int iterator = 0; iterator < iterations; iterator++)
 // END V5 MACROS
+
 
 brain Brain;
 controller Controller1 = controller(primary);
@@ -50,49 +60,55 @@ motor clawMotor = motor(PORT5, ratio36_1, false);
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
 
-
 #pragma endregion VEXcode Generated Robot Configuration
+
 
 void axelSpinForward() {axelMotor.spin(forward, 50, percent);}
 // void axelSpinHold() {axelMotor.spin(forward, 5, percent);}
 void axelSpinStop() {axelMotor.spin(forward, 0, percent);}
-void axelSpinBackward() {axelMotor.spin(forward, -50, percent);}
+void axelSpinBackward() {axelMotor.spin(forward, -80, percent);}
+
 
 //claw motor defaults
 void clawMotorOpen() {clawMotor.spin(forward, -50, percent);}
-void clawMotorClose() {clawMotor.spin(forward, 50, percent);}
+void clawMotorClose() {clawMotor.spin(forward, 80, percent);}
 void clawMotorStop() {clawMotor.spin(forward, 0, percent);}
 // void clawMotorStall() {clawMotor.spin(forward, 0.7, percent);}
 //void clawMotorOpen() {clawMotor.spinFor(forward, 0.5, seconds, 50.0, percent);}
 //void clawMotorClose() {clawMotor.spinFor(forward, -0.5, seconds, 50.0, percent);}
 void turnRobot() {
-  LeftDriveSmart.setVelocity(100, percent);
-  RightDriveSmart.setVelocity(75, percent);
-  LeftDriveSmart.spin(forward);
-  RightDriveSmart.spin(forward);
-  wait (1, seconds);
-  LeftDriveSmart.setVelocity(0, percent);
-  RightDriveSmart.setVelocity(0, percent);
+ LeftDriveSmart.setVelocity(100, percent);
+ RightDriveSmart.setVelocity(75, percent);
+ LeftDriveSmart.spin(forward);
+ RightDriveSmart.spin(forward);
+ wait (1, seconds);
+ LeftDriveSmart.setVelocity(0, percent);
+ RightDriveSmart.setVelocity(0, percent);
 }
 
+
 void brakeRobot() {
-  axelMotor.stop(brakeType::brake);
-  clawMotor.stop(brakeType::hold);
+ axelMotor.stop(brakeType::brake);
+ clawMotor.stop(brakeType::hold);
 }
 /*
 void drive(int v) {
-  LeftDriveSmart.setVelocity(v, percent);
-  RightDriveSmart.setVelocity(v, percent);
+ LeftDriveSmart.setVelocity(v, percent);
+ RightDriveSmart.setVelocity(v, percent);
 }
 */
+/*
 void fullSpeed() {
-  LeftDriveSmart.setVelocity(100, percent);
-  RightDriveSmart.setVelocity(100, percent);
+  // drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 317.5, 117.475, mm, 1);
+  // return Drivetrain;
+ LeftDriveSmart.setVelocity(100, percent);
+ RightDriveSmart.setVelocity(100, percent);
 }
 void noForward() {
-  LeftDriveSmart.setVelocity(0, percent);
-  RightDriveSmart.setVelocity(0, percent);
+ LeftDriveSmart.setVelocity(0, percent);
+ RightDriveSmart.setVelocity(0, percent);
 }
+*/
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -104,11 +120,14 @@ void noForward() {
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
+
 void pre_auton(void) {
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+
+ // All activities that occur before the competition starts
+ // Example: clearing encoders, setting servo positions, ...
 }
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -120,16 +139,17 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+
 void runOnAutonomous(void) {
-  Brain.Screen.print("Running auto");
-  turnRobot();
-  Drivetrain.setDriveVelocity(50, percent);
-  Drivetrain.driveFor(forward, 110, inches);
-  clawMotor.spin(forward, -50, percent);
-  wait (0.75, seconds);
-  Drivetrain.driveFor(forward, -50, inches);
-  
-}
+ Brain.Screen.print("Running auto");
+ clawMotor.spin(forward, 50, percent);
+ turnRobot();
+ Drivetrain.setDriveVelocity(50, percent);
+ Drivetrain.driveFor(forward, 110, inches);
+ clawMotor.spin(forward, -50, percent);
+ wait (0.75, seconds);
+ Drivetrain.driveFor(forward, -50, inches);
+ }
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
 /*                              User Control Task                            */
@@ -140,71 +160,80 @@ void runOnAutonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+
 void runOnDriverControl(void) {
 
-  Brain.Screen.print("Running teleop");
-  axelMotor.setVelocity(0, percent);
 
-  // User control code here, inside the loop
-  while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+ Brain.Screen.print("Running teleop");
+ axelMotor.setVelocity(0, percent);
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    //axel spinning for throwing
-    Controller1.ButtonX.pressed(fullSpeed);
-    LeftDriveSmart.spin(forward);
-    RightDriveSmart.spin(forward);
-    Controller1.ButtonY.pressed(noForward);
-    wait(2, seconds);
-    
-    Controller1.ButtonR1.pressed(axelSpinForward);
-    Controller1.ButtonR1.released(axelSpinStop);
-    // Controller1.ButtonR1.released(axelSpinHold);
-    Controller1.ButtonR2.pressed(axelSpinBackward);
-    Controller1.ButtonR2.released(axelSpinStop);
-    // Controller1.ButtonR2.released(axelSpinHold);
-    
-    
 
-    //open and close claw
-    Controller1.ButtonL1.pressed(clawMotorClose);
-    Controller1.ButtonL2.pressed(clawMotorOpen);
-    //placeholder
-    Controller1.ButtonL1.released(clawMotorStop);
-    Controller1.ButtonL2.released(clawMotorStop);
-    
-    LeftDriveSmart.setVelocity(Controller1.Axis3.position()+Controller1.Axis1.position(), percent);
-    RightDriveSmart.setVelocity(Controller1.Axis3.position()-Controller1.Axis1.position(), percent);
+ // User control code here, inside the loop
+ while (1) {
+   // This is the main execution loop for the user control program.
+   // Each time through the loop your program should update motor + servo
+   // values based on feedback from the joysticks.
 
-    //LeftDriveSmart.spin(forward);
-    //RightDriveSmart.spin(forward); 
 
-    Controller1.ButtonUp.pressed(brakeRobot);
+   // ........................................................................
+   // Insert user code here. This is where you use the joystick values to
+   // update your motors, etc.
+   //axel spinning for throwing
    
-    // ........................................................................
+  //  LeftDriveSmart.spin(forward);
+  //  RightDriveSmart.spin(forward);
+  
+   Controller1.ButtonR1.pressed(axelSpinForward);
+   Controller1.ButtonR1.released(axelSpinStop);
+   // Controller1.ButtonR1.released(axelSpinHold);
+   Controller1.ButtonR2.pressed(axelSpinBackward);
+   Controller1.ButtonR2.released(axelSpinStop);
+   // Controller1.ButtonR2.released(axelSpinHold);
+  
+  
 
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
-  }
+
+   //open and close claw
+   Controller1.ButtonL1.pressed(clawMotorClose);
+   Controller1.ButtonL2.pressed(clawMotorOpen);
+   //placeholder
+   Controller1.ButtonL1.released(clawMotorStop);
+   Controller1.ButtonL2.released(clawMotorStop);
+  
+   LeftDriveSmart.setVelocity(Controller1.Axis3.position()+Controller1.Axis1.position(), percent);
+   RightDriveSmart.setVelocity(Controller1.Axis3.position()-Controller1.Axis1.position(), percent);
+
+
+   LeftDriveSmart.spin(forward);
+   RightDriveSmart.spin(forward);
+
+
+   Controller1.ButtonUp.pressed(brakeRobot);
+ 
+   // ........................................................................
+
+
+   wait(20, msec); // Sleep the task for a short amount of time to
+                   // prevent wasted resources.
+ }
 }
+
 
 //
 // Main will set up the competition functions and callbacks.
 //
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
-  Competition.autonomous(runOnAutonomous);
-  Competition.drivercontrol(runOnDriverControl);
+ // Set up callbacks for autonomous and driver control periods.
+ Competition.autonomous(runOnAutonomous);
+ Competition.drivercontrol(runOnDriverControl);
 
-  // Run the pre-autonomous function.
-  //pre_auton();
 
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
-    wait(0.05, seconds);
-  }
+ // Run the pre-autonomous function.
+ //pre_auton();
+
+
+ // Prevent main from exiting with an infinite loop.
+ while (true) {
+   wait(20, msec);
+ }
 }
